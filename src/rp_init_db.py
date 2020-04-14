9b63db9b0ce352da918c7840893ba0cd5e84edb7
Frank Brunner <frank.brunner@lancos.ch>
2020-04-14 22:42:00 +0200

    db init py angepasst

--- src/rp_init_db.py
+++ src/rp_init_db.py
@@ -2,80 +2,70 @@ import unittest
 import xlrd
 from src.dbconnect import mySql
 from Ressourcenplanning.src.rp_db_struct import *
+import mysql.connector
 
+class InitDB():
+    def __init__(self):
+        self.mydb = mysql.connector.connect(
+            host="localhost",
+            user="dbuser",
+            passwd="34df!5awe"
+        )
+        self.mycursor = self.mydb.cursor()
 
+    def deletDatabase(self):
+        self.mycursor.execute("DROP DATABASE IF EXISTS Ressourcenplanning")
 
+    def createDatabase(self):
+        self.mycursor.execute("CREATE DATABASE Ressourcenplanning")
 
-sql = mySql("dbuser","34df!5awe","Ressourcenplanning")
+    def creatDBUser(self):
+        self.mycursor.execute("CREATE USER IF NOT EXISTS'dbuser'@'localhost' IDENTIFIED BY '34df!5awe'" )
 
-class MyTestCase(unittest.TestCase):
+    def setPermissions(self):
+        self.mycursor.execute("GRANT ALL PRmycursor.executeIVILEGES ON *.* TO 'dbuser'@'localhost'")
 
-    def test_createTableApplications(self):
-
-        sql.executeSql("set foreign_key_checks = 0")
-        #Initial remove alle constrains and delet tables
-        tables=["Projects",
-                "Releases",
-                "Applications",
-                "Data_Employee",
-                "Testobject",
-                "Demand"]
-        for item in tables:
-            sql.tableDelete(item)
-
-
-        #Creat Datata Tables
-        obj_Projects=Projects()
-        self.assertTrue(sql.tableCreate(obj_Projects.tablename,obj_Projects.rows,obj_Projects.types))
+    def createTables(self):
+        sql = mySql("dbuser", "34df!5awe", "Ressourcenplanning")
+        # Creat Datata Tables
+        obj_Projects = Projects()
+        sql.tableCreate(obj_Projects.tablename, obj_Projects.rows, obj_Projects.types)
 
         obj_Releases = Releases()
-        self.assertTrue(sql.tableCreate(obj_Releases.tablename, obj_Releases.rows,obj_Releases.types))
+        sql.tableCreate(obj_Releases.tablename, obj_Releases.rows, obj_Releases.types)
 
         obj_Applications = Applications()
-        self.assertTrue(sql.tableCreate(obj_Applications.tablename, obj_Applications.rows,obj_Applications.types))
+        sql.tableCreate(obj_Applications.tablename, obj_Applications.rows, obj_Applications.types)
 
-        rows = ["Firstname varchar(250)","Secondname varchar(250)","Capacity float(11,1)"]
-        self.assertTrue(sql.tableCreate("Data_Employee", rows))
+        rows = ["Firstname varchar(250)", "Secondname varchar(250)", "Capacity float(11,1)"]
+        sql.tableCreate("Data_Employee", rows)
 
-        #Creat Mapping Tables
+        # Creat Mapping Tables
         obj_Testobject = Testobject()
-        self.assertTrue(sql.tableCreate(obj_Testobject.tablename,obj_Testobject.rows,obj_Testobject.types))
+        sql.tableCreate(obj_Testobject.tablename, obj_Testobject.rows, obj_Testobject.types)
 
         obj_Demand = Demand()
-        self.assertTrue(sql.tableCreate(obj_Demand.tablename, obj_Demand.rows, obj_Demand.types))
+        sql.tableCreate(obj_Demand.tablename, obj_Demand.rows, obj_Demand.types)
 
 
-        #Creat Relations
-
-        # string = "alter table SuperEntity add fforeign key(Projects_id) References Projects(id)," \
-        #          "add foreign key(Releases_id) References SuperEntity_Releases(id)"
-        # sql.executeSql(string)
         #
-        # string = "alter table SuperEntity_Releases add foreign key(SuperEntity_id) References SuperEntity(id)," \
-        #          "add foreign key(Releases_id) References Releases(id)"
-        # sql.executeSql(string)
+        # sql.executeSql("set foreign_key_checks = 0")
+        # #Initial remove alle constrains and delet tables
+        # tables=["Projects",
+        #         "Releases",
+        #         "Applications",
+        #         "Data_Employee",
+        #         "Testobject",
+        #         "Demand"]
+        # for item in tables:
+        #     sql.tableDelete(item)
 
-
-        # sql.recordCreate("Projects", [["Number", "4589"], ["Name", "MusterProject"]])
-        #
-        # sql.recordCreate("Releases", [["Year", "2020"], ["Type", "MDR"],["Description", "MDR04"]])
-        # sql.recordCreate("Releases", [["Year", "2020"], ["Type", "Major"], ["Description", "RE01"]])
-        # sql.recordCreate("Releases", [["Year", "2020"], ["Type", "Major"], ["Description", "RE02"]])
-        # sql.recordCreate("Releases", [["Year", "2020"], ["Type", "Major"], ["Description", "RE03"]])
-        #
-        # sql.recordCreate("SuperEntity", [["Projects_id", "1"], ["Releases_id", "1"]])
-        #
-        # sql.recordCreate("SuperEntity_Releases", [["SuperEntity_id", "1"], ["Releases_id", "1"]])
-        # sql.recordCreate("SuperEntity_Releases", [["SuperEntity_id", "1"], ["Releases_id", "2"]])
-        # sql.recordCreate("SuperEntity_Releases", [["SuperEntity_id", "1"], ["Releases_id", "3"]])
-        # sql.recordCreate("SuperEntity_Releases", [["SuperEntity_id", "2"], ["Releases_id", "4"]])
-        #
-        # #data load
-        # localTemp = "//home/douy/eclipse-workspace/BelegeSortieren/temp/"
-        # workbook = xlrd.open_workbook(localTemp+'import_namen.xlsx')
-        # workbook = workbook.sheet_by_name("Tabelle1")
-        # print (workbook.cell_value(0,0))
-
+init = InitDB()
 
 if __name__ == '__main__':
-    unittest.main(verbosity=2)
+    init.deletDatabase()
+    init.createDatabase()
+    init.creatDBUser()
+    init.createTables()
+
+
